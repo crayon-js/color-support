939e519f2fffd6e4f206774723ef924eed30d250
Im-Beast <franik.mateusz@gmail.com>
2021-05-08 12:48:53 +0200

    add support for deno

---
+++ deno_lib/README.md
@@ -0,0 +1,43 @@
+<font size="6"><p align="center"><b>🖍️ Crayon.js color support</b></p></font>
+<hr />
+
+## :books: About
+##### This package is extension for [crayon.js](https://github.com/crayon-js/crayon) however it can still be used by other packages.
+
+### Installation
+```bash
+npm install @crayon.js/color-support #yarn add @crayon.js/color-support
+```
+
+## Syntax
+```ts
+interface CrayonColorSupport {
+	threeBitColor: boolean
+	fourBitColor: boolean
+	highColor: boolean
+	trueColor: boolean
+}
+```
+
+## Usage
+```ts
+import { getColorSupport, supportedColors, getWindowsVersion } from '@crayon.js/color-support';
+
+const support = getColorSupport(); // detect terminal color support
+const cached = supportedColors(); // cached getColorSupport (it just returns cached object)
+const windowsVersion = getWindowsVersion(); // Reusable function [version (7/8/10...), versionId (14931...)], empty if detected system is not Windows
+```
+
+## Usage with crayon.js
+```ts
+import crayon from 'crayon.js'; // it'll still work with packages that extend crayon instance as its config is global
+import { getColorSupport } from '@crayon.js/color-support';
+
+crayon.config.colorSupport = getColorSupport();
+```
+
+## :handshake: Contributing
+#### Feel free to add any commits, issues and pull requests
+
+## :memo: Licensing
+#### This project is available under MIT License conditions.
---
+++ deno_lib/index.ts
@@ -0,0 +1,78 @@
+import { execSync } from 'child_process DENOIFY: DEPENDENCY UNMET (BUILTIN)'
+import os from 'https://deno.land/std@0.85.0/node/os.ts'
+import { CrayonColorSupport } from './types.ts'
+
+export const getWindowsVersion = (): number[] => {
+	if (process.platform == 'win32') {
+		const [version, , versionId] = os.release().split('.')
+		return [Number(version), Number(versionId)]
+	}
+	return []
+}
+
+let trueColor = false
+let highColor = false
+let threeBitColor = false
+let fourBitColor = false
+
+export const supportedColors = (): CrayonColorSupport => ({
+	trueColor,
+	highColor,
+	threeBitColor,
+	fourBitColor,
+})
+
+const CIs = [
+	'TRAVIS',
+	'CIRCLECI',
+	'GITHUB_ACTIONS',
+	'GITLAB_CI',
+	'BUILDKITE',
+	'DRONE',
+	'APPVEYOR',
+]
+
+export const getColorSupport = (): CrayonColorSupport => {
+	if (process.env.NO_COLOR) {
+		threeBitColor = fourBitColor = highColor = trueColor = false
+		return supportedColors()
+	}
+
+	switch (process.env.COLORTERM) {
+		case 'truecolor':
+			threeBitColor = fourBitColor = highColor = trueColor = true
+			return supportedColors()
+		// are there other settings for that variable?
+	}
+
+	if (/-?256(color)?/gi.test(process.env.TERM || '')) {
+		fourBitColor = highColor = true
+		return supportedColors()
+	}
+
+	if (process.env.CI && CIs.some((CI) => !!process.env[CI])) {
+		threeBitColor = fourBitColor = true
+		return supportedColors()
+	}
+
+	if (process.env.COLORTERM) {
+		threeBitColor = fourBitColor = true
+		return supportedColors()
+	}
+
+	const winVersion = getWindowsVersion()
+	if (winVersion.length) {
+		// https://devblogs.microsoft.com/commandline/24-bit-color-in-the-windows-console/
+		threeBitColor = fourBitColor = highColor = trueColor = winVersion[1] > 14931
+		return supportedColors()
+	}
+
+	const tputColors = Number(execSync('tput colors')) || 0
+
+	threeBitColor = tputColors >= 4
+	fourBitColor = tputColors >= 8
+	highColor = tputColors >= 256
+	trueColor = tputColors >= 16777216
+
+	return supportedColors()
+}
